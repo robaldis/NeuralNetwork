cecce46986fd408cc8875d109729643192d41602
Robert <robertaldis@gmail.com>
2019-08-06 20:21:21 +0100

    Training the neural network

--- NeuralNetowrkLibrary/main.py
+++ NeuralNetowrkLibrary/main.py
@@ -1,15 +1,45 @@
-import neuralNetwork2 as nn
+import neuralNetwork as nn
 from numpy import *
+import random
+from time import time, localtime, ctime
 
 
 def main():
+    brain = nn.NeuralNetwrok(2,2,1, 0.1)
 
-    input = [1,0,1,0]
 
-    brain = nn.NeuralNetwrok(4,5,6)
-    output = brain.feedForward(input)
-    print (output)
 
+
+    input = [1,0],[0,1],[0,0],[1,1]
+    target = [1],[1],[0],[0]
+
+    startTime = time()
+
+    print("Starting to train the network")
+    print(f"Time Started: {ctime(startTime)}")
+
+    for i in range (100000):
+        index = random.randint(0,3)
+        brain.gradientDecent(input[index], target[index])
+
+    print("Done!!")
+    endTime = time()
+    timeTaken = endTime - startTime
+
+    print(f"Time ended: {ctime(endTime)}")
+    print(f"Time Taken: {round((timeTaken / 60), 2)} minutes\n")
+
+    output = brain.feedForward([0,0])
+    if (output > 0.5):
+        print("TRUE")
+    else:
+        print ("FALSE")
+
+    output1 = brain.feedForward([0,1])
+    output2 = brain.feedForward([1,0])
+    output3 = brain.feedForward([0,0])
+    output4 = brain.feedForward([1,1])
+    print(f'{output1}\n{output2}\n{output3}\n{output4}')
 
 
 if (__name__ == "__main__"):
--- NeuralNetowrkLibrary/neuralNetwork.py
+++ NeuralNetowrkLibrary/neuralNetwork.py
@@ -4,9 +4,14 @@ import math
 import scipy.special
 
 
+def matrixAdd(a, b):
+    for i in range((size(a, 0))):
+        for j in range(size(a, 1)):
+            a[i][j] += b[i][j]
+
 class NeuralNetwrok (object):
 
-    def __init__(self,input_nodes,hidden_nodes, output_nodes):
+    def __init__(self,input_nodes,hidden_nodes, output_nodes,lr):
         self.input_nodes = input_nodes
         self.hidden_nodes = hidden_nodes
         self.output_nodes = output_nodes
@@ -14,20 +19,26 @@ class NeuralNetwrok (object):
         # Make the weights between the input and hidden and hidden and output
         self.weights_ih = ndarray(shape = (self.hidden_nodes, self.input_nodes))
         self.weights_ho = ndarray(shape = (self.output_nodes, self.hidden_nodes))
-
         # randomise the weights
         self.weights_ih = self.randomWeights(self.weights_ih)
         self.weights_ho = self.randomWeights(self.weights_ho)
 
+        # Create the bias arrays
         self.bias_h = ndarray(shape = (self.hidden_nodes,1))
         self.bias_o = ndarray(shape = (self.output_nodes, 1))
         self.bias_h = self.randomWeights(self.bias_h)
         self.bias_o = self.randomWeights(self.bias_o)
+        # self.bias_h = matrix.transpose(self.bias_h)
+        # self.bias_o = matrix.transpose(self.bias_o)
+
+
+        self.lr = lr
 
         # activation function is the sigmoid function
         self.activation_function = lambda x: scipy.special.expit(x)
 
         pass
+
 
     def feedForward(self, inputArray):
         # convert inputs list to 2d array
@@ -39,13 +50,72 @@ class NeuralNetwrok (object):
         hiddenOutput = self.activation_function(hiddenInput)
         # hiddenOutput = matrix.transpose(hiddenOutput)
 
+
         # Calclate signals into the final layer
         finalInput = dot(self.weights_ho, hiddenOutput)
         finalInput = finalInput + self.bias_o
         finalOutput = self.activation_function(finalInput)
 
+        for i in range((size(finalOutput, 0))):
+            for j in range(size(finalOutput, 1)):
+                finalOutput[i][j] = round(finalOutput[i][j], 2)
+
+
         # Sending it back to the caller
         return finalOutput
+
+    def gradientDecent(self, inputArray, targetArray):
+
+        # convert inputs list to 2d array
+        input = array(inputArray, ndmin=2).T
+
+        # Calculate singnals into the hidden layer
+        hiddenInput = dot(self.weights_ih, input)
+        hiddenInput = hiddenInput + self.bias_h
+        hiddenOutput = self.activation_function(hiddenInput)
+        # hiddenOutput = matrix.transpose(hiddenOutput)
+
+        # Calclate signals into the final layer
+        finalInputs = dot(self.weights_ho, hiddenOutput)
+        finalInputs = finalInputs + self.bias_o
+        finalOutputs = self.activation_function(finalInputs)
+
+        target = array(targetArray, ndmin=2).T
+
+        # Calculate the errors for the output layer
+        outputErrors = target - finalOutputs
+        outputGradiants = finalOutputs * (1- finalOutputs)
+        outputGradiants = outputGradiants * outputErrors
+        outputGradiants = outputGradiants * self.lr
+
+        # Calclate deltas
+        hidden_T = matrix.transpose(hiddenOutput)
+        weights_ho_deltas = outputGradiants * hidden_T
+
+        # Change the weights by the deltas
+        self.weights_ho += weights_ho_deltas
+        # Change the bias with the gradiant
+        self.bias_o += outputGradiants
+
+        # Calculate the hidden layer errors
+        who_T = matrix.transpose(self.weights_ho)
+        hiddenErrors = who_T * outputErrors
+
+        # Hidden layer gradiant
+        hiddenGradiants = hiddenOutput * ( 1 - hiddenOutput)
+        hiddenGradiants = hiddenGradiants * hiddenErrors
+        hiddenGradiants = hiddenGradiants * self.lr
+
+        # Calculate deltas
+        input_t = matrix.transpose(input)
+        weights_ih_deltas = hiddenGradiants * input_t
+
+        # change the weights by the deltas
+        self.weights_ih += weights_ih_deltas
+        # Change the bias with the gradiant
+        self.bias_h += hiddenGradiants
+
+
 
 
     def randomWeights(self, n):
